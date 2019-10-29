a23d85a197d3d7d031df74269c5bb8221273aef3
Hoda Ahmad <ccs7vp@github.com>
2019-10-29 19:22:38 -0400

    Initial commit.

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+
+var friends = [
+	{
+		name: "Emily",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Barry",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	},
+	{
+		name: "Max",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Laurel",
+		scores: [4, 5, 5, 3, 1, 2, 1, 3, 5, 4]
+	},
+	{
+		name: "Bill",
+		scores: [3, 5, 2, 2, 1, 3, 4, 4, 5, 1]
+	}
+];
+
+module.exports = friends;
